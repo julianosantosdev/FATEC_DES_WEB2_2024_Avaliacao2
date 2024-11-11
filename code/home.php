d7e35fd5461576007f98ceadf1b383dd9900fd35
Juliano Santos <julianosantosdev@outlook.com>
2024-11-11 18:29:43 -0300

    first commit

---
+++ code/home.php
@@ -0,0 +1,25 @@
+<?php
+require('classes/login.php');
+$validador = new Login();
+$validador->verificar_logado();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Vagas de Estágio</title>
+</head>
+<body>
+    <center>
+        <h2>Vagas de Estágio</h2>
+    </center>
+    
+    <br>
+    <!--
+    Espera-se botões aqui
+    -->
+    <a href="login.php">Logout</a>
+</body>
+</html>
